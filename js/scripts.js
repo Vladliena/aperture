c4d43821e5592a8553ac10292efeead3dc3161b7
vladliena <sotnikovavladlena@gmail.com>
2022-06-21 18:18:43 +0300

    added adaptive

--- js/scripts.js
+++ js/scripts.js
@@ -10,11 +10,16 @@ $(document).ready(function(){
 
         responsive:{
             0:{
-                items:1
+                items:2,
             },
-
+            600:{
+                items:3,
+            },
+            800:{
+                items:4,
+            },
             1000:{
-                items:4
+                items:5
             }
         }
     });
